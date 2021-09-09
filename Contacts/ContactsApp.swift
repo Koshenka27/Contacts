f999c4d7d6a962c60435c0913f909eb668bd4695
Logan Koshenka <koshenka27@gmail.com>
2021-09-09 06:38:37 -0400

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Logan Koshenka on 9/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
